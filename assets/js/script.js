01637f01170f23d9073bd9e3a9906e61f8ed56c9
Mohamed Hany <MohamedEgy246@gmail.com>
2025-09-01 18:16:35 +0300

    fixing some responsive bugs in the navbar.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector(".menu");
 const header = document.querySelector(".header");
-
+const closePromotionBtn = document.querySelector(".close-promotion");
 menu.addEventListener("click", () => {
   header.classList.toggle("open");
+});
+closePromotionBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".promotion").remove();
 });
