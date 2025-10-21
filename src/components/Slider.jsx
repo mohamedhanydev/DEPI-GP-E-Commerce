91f1be3abf3ab417410e0d30681a0c731b26e3b5
Mohamed Hany <MohamedEgy246@gmail.com>
2025-10-22 02:05:38 +0300

    fix images paths'.

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,13 @@ import { useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import "../styles/slider.css";
+import inst1 from "../assets/inst-1-min.webp";
+import inst2 from "../assets/inst-2-min.webp";
+import inst3 from "../assets/inst-3-min.webp";
+import inst4 from "../assets/inst-4-min.webp";
+import inst5 from "../assets/inst-5-min.webp";
 
+const images = [inst1, inst2, inst3, inst4, inst5];
 export default function Slider() {
   const [emblaRef] = useEmblaCarousel({ loop: true, playOnInit: true }, [
     Autoplay(),
@@ -11,13 +17,10 @@ export default function Slider() {
     <div className="container">
       <div className="embla" ref={emblaRef}>
         <div className="embla__container">
-          {Array.from({ length: 6 }).map((_, i) => (
+          {images.map((image, i) => (
             <div className="embla__slide" key={i}>
               <a href="#" className="insta-item">
-                <img
-                  src={`../assets/inst-${i + 1}-min.webp`}
-                  alt={`Instagram ${i + 1}`}
-                />
+                <img src={image} alt={`Instagram ${i + 1}`} />
               </a>
             </div>
           ))}
