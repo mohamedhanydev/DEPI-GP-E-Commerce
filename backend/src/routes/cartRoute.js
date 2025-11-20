a01c3f410c67b9817411bcc1eb4c88f93dccf035
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-20 23:27:34 +0200

    import cartController into cartRoute

--- backend/src/routes/cartRoute.js
+++ backend/src/routes/cartRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const cartController = require("../controllers/cartController");
+router
+  .route("/")
+  .get(cartController.getAllCartItems)
+  .delete(cartController.deleteAllCartItems);
 
-router.route("/").get(getCartItems).delete(deleteAllCartItems);
-
-router.route("/items").post(addCartItem);
+router.route("/items").post(cartController.addCartItem);
 router
   .route("/items/:itemId")
-  .patch(updateCartItemQuantity)
-  .delete(deleteOneCartItem);
+  .patch(cartController.updateCartItemQuantity)
+  .delete(cartController.deleteOneCartItem);
