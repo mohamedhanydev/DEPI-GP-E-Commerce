b2a0a2feb8083faa2ffd9f5257b523b69b781d5f
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-20 18:20:12 +0200

    Implement getAllProducts controller

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,6 +1,13 @@
+const productsService = require("../services/productsService");
 const getAllProducts = (req, res) => {
-  // return all products
-  res.send("all products");
+  try {
+    const allProducts = productsService.getAllProducts();
+    res.status(200).send({ status: "OK", data: allProducts });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", message: error.message });
+  }
 };
 const getOneProduct = (req, res) => {
   //return specific product
