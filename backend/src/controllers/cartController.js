f63d192663726395e89cbb9e03e090feacff7498
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-20 23:15:29 +0200

    Implement deleteAllCartItems handler function, and add userId to the getAllCartItems handler

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -1,7 +1,19 @@
 const getAllCartItems = async (req, res) => {
   try {
-    const allCartItems = await cartService.getAllCartItems();
+    const userId = req.user.id;
+    const allCartItems = await cartService.getAllCartItems(userId);
     res.status(200).send({ status: "OK", data: allCartItems });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", message: error.message });
+  }
+};
+const deleteAllCartItems = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const deleteAllCartItems = await cartService.deleteAllCartItems(userId);
+    res.status(200).send({ status: "OK", data: deleteAllCartItems });
   } catch (error) {
     res
       .status(error.status || 500)
