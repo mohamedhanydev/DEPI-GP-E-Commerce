b6cef99fc9f218a1894d832806dc18d325e9e241
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-20 23:20:10 +0200

    implement addCartItem handler function

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -20,3 +20,19 @@ const deleteAllCartItems = async (req, res) => {
       .send({ status: "FAILED", message: error.message });
   }
 };
+const addCartItem = async (req, res) => {
+  try {
+    const userId = req.user.id,
+      { productId, quantity } = req.body;
+    const addedCartItem = await cartService.addCartItem(
+      userId,
+      productId,
+      quantity
+    );
+    res.status(200).send({ status: "OK", data: addedCartItem });
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .send({ status: "FAILED", message: error.message });
+  }
+};
