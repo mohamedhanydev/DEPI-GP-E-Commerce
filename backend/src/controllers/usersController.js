af3128917b7cd333b6fe556bfcd29915cc65ce6c
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-02 23:48:35 +0200

    generate token and send it back to client

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -26,11 +26,11 @@ const login = async (req, res) => {
       return res
         .status(400)
         .json({ message: "missing email or username or password or role" });
-    const loginUser = await userService.login({
+    const token = await userService.login({
       email,
       password,
     });
-    res.status(200).json({ message: "login done!" });
+    res.status(200).json({ message: "login done!", token });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message });
   }
