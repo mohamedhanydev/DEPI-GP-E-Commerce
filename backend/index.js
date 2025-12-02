d42e9fbd247d83d49f60a64c43ff16a5a3dcdf46
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-02 20:37:48 +0200

    change the local port working on to 3700

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const connectDB = require("./src/database/connect");
 const products = require("./src/routes/products");
 const users = require("./src/routes/users");
-const PORT = process.env.PORT || 3600;
+const PORT = process.env.PORT || 3700;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
