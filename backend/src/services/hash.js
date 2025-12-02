0809badeade8d15b6348d4a2c966584a2258870d
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-02 19:10:12 +0200

    implement hashPassword and comparePassword functions

---
+++ backend/src/services/hash.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcryptjs");
+
+const hashPassword = async (plainPassword) => {
+  const saltRounds = 10;
+  return await bcrypt.hash(plainPassword, saltRounds);
+};
+
+const comparePassword = async (plainPassword, hashedPassword) => {
+  return await bcrypt.compare(plainPassword, hashedPassword);
+};
+module.exports = { hashPassword, comparePassword };
