3f0ae4e1aeca02dd1e1f6af548274a1d5df2a871
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-03 14:43:39 +0200

    add expire duration for token

--- backend/src/services/jwtService.js
+++ backend/src/services/jwtService.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET);
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 const verifyToken = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
