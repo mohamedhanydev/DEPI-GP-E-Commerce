637a1933e5a3c06139b40a88b4ee954eef6df3fd
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-04 16:55:52 +0200

    Implement cart route

---
+++ backend/src/routes/cartRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const cartController = require("../controllers/cartController");
+
+router
+  .route("/")
+  .post(cartController.addItemToCart)
+  .get(cartController.getCartItems)
+  .delete(cartController.deleteAllCartItems);
+
+router.route("/:itemId").delete(cartController.deleteOneCartItem);
+module.exports = router;
