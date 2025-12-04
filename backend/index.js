26d59bd637440c3843489296c1f8f1084d71cd9a
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-04 17:35:11 +0200

    Add usersRoute as a protected route

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,18 @@ const cors = require("cors");
 const app = express();
 const connectDB = require("./src/database/connect");
 const productsRoute = require("./src/routes/productsRoute");
-const usersRoute = require("./src/routes/usersRoute");
+const authRoute = require("./src/routes/authRoute");
 const cartRoute = require("./src/routes/cartRoute");
+const usersRoute = require("./src/routes/usersRoute");
+const authMiddleware = require("./src/middlwares/authMiddleware");
 const PORT = process.env.PORT || 3700;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth/", usersRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/products", productsRoute);
-app.use("/api/cart", cartRoute);
+app.use("/api/cart", authMiddleware, cartRoute);
+app.use("/api/users", authMiddleware, usersRoute);
 async function startServer() {
   try {
     await connectDB();
