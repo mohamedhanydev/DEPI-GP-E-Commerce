2ac04f0190c8da06aada7dc0e5df6bfc17b53b69
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-06 15:03:10 +0200

    add stripe route and orders route

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const productsRoute = require("./src/routes/productsRoute");
 const authRoute = require("./src/routes/authRoute");
 const cartRoute = require("./src/routes/cartRoute");
 const usersRoute = require("./src/routes/usersRoute");
+const stripeRoute = require("./src/routes/stripeRoute");
+const ordersRoute = require("./src/routes/ordersRoute");
 const authMiddleware = require("./src/middlwares/authMiddleware");
 const PORT = process.env.PORT || 3700;
 app.use(cors());
@@ -15,6 +17,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/cart", authMiddleware, cartRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/stripe", stripeRoute);
+app.use("/api/orders", ordersRoute);
 async function startServer() {
   try {
     await connectDB();
