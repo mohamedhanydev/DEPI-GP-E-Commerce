68b66e5799eb4f7b5ff27f3ad206b2e59a205607
Mohamed Hany <MohamedEgy246@gmail.com>
2025-12-06 16:36:27 +0200

    fix bug: restrict non admin users from accessing /admin

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/usersController");
+const authController = require("../controllers/authController");
 
-router.post("/register", userController.register);
-router.post("/login", userController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 module.exports = router;
